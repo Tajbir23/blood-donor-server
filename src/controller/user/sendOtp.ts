28ccc56000527aba7381a2ad8d70c578afcec3e3
Tajbir23 <studenttajbirislam@gmail.com>
2025-03-28 13:07:18 +0600

    fix: send otp

---
+++ src/controller/user/sendOtp.ts
@@ -0,0 +1,18 @@
+import sendEmail from "../email/sendEmail";
+
+export const otpMap = new Map<string, string>()
+
+export const generateOTP = (email: string) => {
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    otpMap.set(email, otp)
+    return otp
+}
+
+const sendOtp = async(email: string) => {
+    
+    const otp = await generateOTP(email)
+    const data = await sendEmail({email, subject: "Verify Email", templateType: "verifyEmail", templateData: {otp: otp}})
+    return data
+}
+
+export default sendOtp
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -5,14 +5,12 @@ import imageUpload from "../handler/fileUpload/imageUpload";
 import loginUser from "../controller/user/loginUser";
 import verifyJwt from "../handler/validation/verifyJwt";
 import logoutUser from "../controller/user/logoutUser";
-import sendEmail from "../controller/email/sendEmail";
-import verifyEmail from "../controller/user/verifyEmail";
+
 
 const userRouter = Router();
 
 userRouter.post('/register', loginLimiter, imageUpload, createUser)
 userRouter.post('/login', loginLimiter, loginUser)
 userRouter.get("/logout", verifyJwt, logoutUser)
-userRouter.post("/verify-email", loginLimiter, verifyEmail)
 
 export default userRouter;
