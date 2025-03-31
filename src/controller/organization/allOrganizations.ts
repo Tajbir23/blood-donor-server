1414cd4e89c4f276760a1ccc7f97033916ef06b7
Tajbir23 <studenttajbirislam@gmail.com>
2025-03-31 14:03:35 +0600

    test

---
+++ src/controller/organization/allOrganizations.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import organizationModel from "../../models/organization/organizationSchema";
+
+const allOrganizations = async (req: Request, res: Response) => {
+    const { organization } = req.query
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const query: { organizationName?: { $regex: RegExp } } = {};
+    if (organization && typeof organization === 'string') {
+        query.organizationName = { $regex: new RegExp(organization, 'i') };
+    }
+    const organizations = await organizationModel.find(query).skip(startIndex).limit(limit);
+    const totalOrganizations = await organizationModel.countDocuments(query);
+    res.status(200).json( {organizations, totalOrganizations});
+}
+
+export default allOrganizations;
--- src/router/organizationRoute.ts
+++ src/router/organizationRoute.ts
@@ -3,11 +3,13 @@ import registerOrg from "../controller/organization/registerOrg";
 import verifyJwt from "../handler/validation/verifyJwt";
 import { createImageUpload } from "../handler/fileUpload/imageUpload";
 import myOrganizations from "../controller/organization/myOrganizations";
+import allOrganizations from "../controller/organization/allOrganizations";
 
 const organizationRouter = Router();
 
 const organizationLogoUpload = createImageUpload('organization')
 organizationRouter.post('/register', verifyJwt, organizationLogoUpload, registerOrg)
 organizationRouter.get('/my_organizations', verifyJwt, myOrganizations)
+organizationRouter.get('/organizations', allOrganizations)
 export default organizationRouter;
 
